c37751f361b4fd8c7cdbfe018d7861f5f0b9e818
tmomy <datmomy@gmail.com>
2017-09-21 08:22:24 +0800

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
+import { DemoGaridBasicComponent} from './demo.garid-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoGaridBasicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/demo.garid-basic.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nz-demo-grid-basic',
+  templateUrl: './demo.garid-basic.component.html',
+  styleUrls: ['./demo.garid-basic.component.css']
+})
+export class DemoGaridBasicComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+}
